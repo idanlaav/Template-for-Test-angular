88baea2cb311c5566fba78a779602a71c87fd871
idanlaav <idanlaav.1@gmail.com>
2022-07-11 21:10:29 +0300

    test commit

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    console.log(123)
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    .catch(err => console.error(err));
